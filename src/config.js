ce44d94a71bc1ae66b09a6d87f898a9aecd30dff
Kelvin-orcasita <klvorcasita@gmail.com>
2023-12-11 22:24:21 -0500

    update

---
+++ src/config.js
@@ -0,0 +1,20 @@
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyDmN_4m0CTy6vX_8WzMExXEAEPM6qxkFnk",
+//     authDomain: "fdfgdgfgd-f4c77.firebaseapp.com",
+//     projectId: "fdfgdgfgd-f4c77",
+//     storageBucket: "fdfgdgfgd-f4c77.appspot.com",
+//     messagingSenderId: "930540741919",
+//     appId: "1:930540741919:web:56454aa9a513c69d9abc2a"
+// };
+
+
+
+// Mi cuenta
+export const firebaseConfig = {
+    apiKey: "AIzaSyDgerjcQFN4MWT3dQezyr9L_nwKJl0XqWg",
+    authDomain: "newproject-d9b6f.firebaseapp.com",
+    projectId: "newproject-d9b6f",
+    storageBucket: "newproject-d9b6f.appspot.com",
+    messagingSenderId: "80653108923",
+    appId: "1:80653108923:web:99365494d146ff40944b9e"
+};
